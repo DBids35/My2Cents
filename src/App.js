a4691f3e7767bb39738a1e8ef2cf3c40c3c7d011
DBids35 <drew.bidlen@yahoo.com>
2017-11-16 23:37:20 -0500

    Added basic poll and "Create poll" button

--- src/App.js
+++ src/App.js
@@ -17,20 +17,38 @@ class Header extends Component {
     return(
         <div className="header">
           <div className="headerLogo">
-            <img src={logo} className="logo"/>
+            <img src={logo} className="logo" alt="Logo"/>
           </div>
           <div className="headerTitle">
             <h1> My2Cents </h1>
           </div>
-          <div className="headerUser">
-            <h3> Drew </h3>
-            <p> 10 </p>
-          </div>
+          <UserDisplay/>
         </div>
       )
   }
 }
 
+class PollButton extends Component{
+  render(){
+    return (
+
+      <button type="button" className="createPollButton">Create new Poll</button>
+
+      )
+  }
+}
+class Poll extends Component {
+  render(){
+    return(
+      <div className="pollContainer">
+      <h3 className="pollTitle"> Buy Google </h3>
+      <p className="pollText"> I think that we should buy a bunch of shares of Google because of X Y and Z. What do you think? Do you agree or disagree?</p>
+      <button type="button" className="agree">Agree</button>
+      <button type="button" className="disagree">Disagree</button>
+      </div>
+      )
+  }
+}
 class App extends Component {
 
   componentDidMount() {
@@ -40,9 +58,8 @@ class App extends Component {
     return (
       <div className="App">
         <Header/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <PollButton/>
+        <Poll/>
       </div>
     );
   }
