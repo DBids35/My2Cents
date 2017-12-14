52d48f38b9d7f134a3d4f8a873b3d1db387b6768
DBids35 <drew.bidlen@yahoo.com>
2017-12-14 14:48:40 -0500

    Single mutually exclusive voting

--- src/components/Poll.jsx
+++ src/components/Poll.jsx
@@ -5,22 +5,37 @@ import PollCountdown from './PollCountdown';
 export default class Poll extends Component {
   state = {
     agreeVotes:0,
-    disagreeVotes:0
+    disagreeVotes:0,
+    userVoteStatus:0
   };
 
   componentDidMount(){}
 
-  incrementAgree(){
-    this.setState(prevState => ({
-      agreeVotes: prevState.agreeVotes + 1,
-    }));
+  incrementAgree=()=>{
+    if (this.state.userVoteStatus==0){
+      this.setState(prevState => ({agreeVotes: prevState.agreeVotes + 1,}))
+      this.setState({userVoteStatus:1})
+    }
+    else if (this.state.userVoteStatus==-1){
+      this.setState(prevState => ({agreeVotes: prevState.agreeVotes + 1,}))
+      this.setState(prevState => ({disagreeVotes: prevState.disagreeVotes - 1,}))
+      this.setState({userVoteStatus:1})
+    }
+    
     this.props.onClick();
   }
 
-  incrementDisagree(){
-    this.setState(prevState => ({
-      disagreeVotes: prevState.disagreeVotes + 1,
-    }));
+  incrementDisagree=()=>{
+    if (this.state.userVoteStatus==0){
+      this.setState(prevState => ({disagreeVotes: prevState.disagreeVotes + 1,}))
+      this.setState({userVoteStatus:-1})
+    }
+    else if (this.state.userVoteStatus==1){
+      this.setState(prevState => ({disagreeVotes: prevState.disagreeVotes + 1,}))
+      this.setState(prevState => ({agreeVotes: prevState.agreeVotes - 1,}))
+      this.setState({userVoteStatus:-1})
+    }
+    
     this.props.onClick();
   }
 
